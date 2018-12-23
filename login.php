6a8dc45ca9f61ff7b086ced7072b89b4795e5640
Walid SIA <walidsiadev@outlook.fr>
2018-12-23 02:58:01 +0100

    add lib php jwt

--- login.php
+++ login.php
@@ -4,8 +4,10 @@
 
 $email = $_POST['email'];
 $passe = sha1($_POST['passe']);
-
+header("location: http://localhost/jwt-server/index.php?token:$email,$passe");
+/*
 echo $email; 
 echo '<pre>';
 echo $passe
+*/
 ?>
